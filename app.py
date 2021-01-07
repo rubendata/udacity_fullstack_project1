faba7032a27da2ef3713400e05adfe99e7e57930
rubendata <info@rubensimon.de>
2021-01-07 21:55:53 +0100

    added route  for /artists

--- app.py
+++ app.py
@@ -341,19 +341,18 @@ def delete_venue(venue_id):
 
 #  Artists
 #  ----------------------------------------------------------------
-@app.route('/artists')
+@app.route('/artists') #fertig
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data=[]
+  artists = Artist.query.all()
+  for artist in artists:
+    record={
+      "id": artist.id,
+      "name": artist.name
+    }
+    data.append(record)
+    
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
